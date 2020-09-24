cbef1e7bf219c34fd0110f44a384a7e02d1a7231
Alexsey199 <71441091+Alexsey199@users.noreply.github.com>
2020-09-24 19:33:11 +0300

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]    INT           IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (50) NULL,
+    [Price] NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
